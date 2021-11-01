7c93301d2576465a0d955855412bbd4917dc7d4a
Emilia Czyrak <emiczyrak@gmail.com>
2021-11-01 13:06:03 +0100

    lab02

--- lab02emi/zadanie9.py
+++ lab02emi/zadanie9.py
@@ -1,7 +1,4 @@
-from typing import Text
-import file_manager
 import file_manager as fm
-
 test = fm.FileManager("lab02emi/zadanie9test.txt")
 test.read_file()
 test.update_file("\nwriting to file.")
